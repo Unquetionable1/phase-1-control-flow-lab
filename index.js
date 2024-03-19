8b416c11c85e2e18a227d8c17f21e8d47b55f7a7
Edmound <eddnjoori@gmail.com>
2024-03-19 13:59:31 +0300

    Adds selection function

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(my_distance){
   // Write your code here!
+  //less than 400" cost free
+  if(my_distance<400){
+    return 'This one is on me!'
+  }else
+  //between 400 & 2000" cost=20
+  if(my_distance>400 && my_distance<2000){
+    return 'That will be twenty bucks.'
+  }else
+  //over 2000" cost=30
+  if(2000<my_distance && my_distance<2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (my_distance>2500 ){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city ==='NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default: 
+      return 'Bye.'
+      break;
+  }
 }
